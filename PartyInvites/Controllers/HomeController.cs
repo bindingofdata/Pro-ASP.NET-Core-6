a9bfedde68dab778befc857eb8cfa5e2e077f6cd
Hunter Curren <hcurren82@gmail.com>
2025-01-14 11:26:28 -0800

    Added and hooked up initial RSVP form.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -12,5 +12,10 @@ namespace PartyInvites.Controllers
         {
             return View();
         }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
     }
 }
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace PartyInvites.Models
+{
+    public sealed class GuestResponse
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
