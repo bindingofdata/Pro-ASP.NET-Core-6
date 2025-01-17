3761c91c247ae7c769cdf8e5f576914738e29079
Hunter Curren <hcurren82@gmail.com>
2025-01-17 13:05:24 -0800

    Created and completed initial set up for the Sports Store project, including the Unit Test project.

---
+++ SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ SportsStore/Program.cs
@@ -0,0 +1,19 @@
+namespace SportsStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseStaticFiles();
+            app.MapDefaultControllerRoute();
+
+            app.Run();
+        }
+    }
+}
