3760b597162d0e526ce12caf0c1e5dcbc13f87d1
Hunter Curren <hcurren82@gmail.com>
2025-01-22 15:00:08 -0800

    Begin pagination support: Added support to HomeController and added unit test.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -7,12 +7,17 @@ namespace SportsStore.Controllers
     public sealed class HomeController : Controller
     {
         private readonly IStoreRepository _repository;
+        public int PageSize { get; set; } = 4;
 
         public HomeController(IStoreRepository repo)
         {
             _repository = repo;
         }
 
-        public IActionResult Index() => View(_repository.Products);
+        public IActionResult Index(int productPage = 1) =>
+            View(_repository.Products
+                .OrderBy(product => product.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
     }
 }
