ef2f3b4afbf2a2dc551153bdf85a4cbebdde2f79
Hunter Curren <hcurren82@gmail.com>
2025-01-22 14:35:44 -0800

    Updated view to list products and added first Unit test.

---
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        private void Can_Use_Repo()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" }
+            }).AsQueryable<Product>());
+
+            HomeController controller = new(mock.Object);
+
+            // Act
+            IEnumerable<Product>? result = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+    }
+}
