b001b5f0a2e98a1e531ef8936e645f6b2a39f801
Hunter Curren <hcurren82@gmail.com>
2025-01-22 16:41:55 -0800

    Continue pagination support: Added view model PagingInfo and helper class PageLinkTagHelper, along with PageLinkTagHelper unit tests.

---
+++ SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using Moq;
+
+using SportsStore.Infrastructure;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Tests
+{
+    public class PageLinkTagHelperTests
+    {
+        [Fact]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange
+            const string PAGE_ONE_STRING = "Test/Page1";
+            const string PAGE_TWO_STRING = "Test/Page2";
+            const string PAGE_THREE_STRING = "Test/Page3";
+            Mock<IUrlHelper> urlHelper = new();
+            urlHelper.SetupSequence(x =>
+                x.Action(It.IsAny<UrlActionContext>()))
+                    .Returns(PAGE_ONE_STRING)
+                    .Returns(PAGE_TWO_STRING)
+                    .Returns(PAGE_THREE_STRING);
+
+            Mock<IUrlHelperFactory> urlHelperFactory = new();
+            urlHelperFactory.Setup(f =>
+                f.GetUrlHelper(It.IsAny<ActionContext>()))
+                    .Returns(urlHelper.Object);
+
+            Mock<ViewContext> viewContext = new();
+
+            PageLinkTagHelper helper = new(urlHelperFactory.Object)
+            {
+                PageModel = new()
+                {
+                    TotalItems = 28,
+                    ItemsPerPage = 10,
+                    CurrentPage = 2
+                },
+                ViewContext = viewContext.Object,
+                PageAction = "Test"
+            };
+
+            TagHelperContext ctx = new(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                string.Empty);
+
+            Mock<TagHelperContent> content = new();
+            TagHelperOutput output = new(
+                "div",
+                new TagHelperAttributeList(),
+                (cache, encoder) =>
+                    Task.FromResult(content.Object));
+
+            // Act
+            helper.Process(ctx, output);
+
+            // Assert
+            Assert.Equal(
+                $@"<a href=""{PAGE_ONE_STRING}"">1</a>" +
+                $@"<a href=""{PAGE_TWO_STRING}"">2</a>" +
+                $@"<a href=""{PAGE_THREE_STRING}"">3</a>",
+                output.Content.GetContent());
+        }
+    }
+}
---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using SportsStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public sealed class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext? ViewContext { get; set; }
+        public PagingInfo? PageModel { get; set; }
+        public string PageAction { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (ViewContext != null && PageModel != null)
+            {
+                IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+                TagBuilder result = new TagBuilder("div");
+                for (int i = 1; i <= PageModel.TotalPages; i++)
+                {
+                    TagBuilder tag = new TagBuilder("a");
+                    tag.Attributes["href"] = urlHelper.Action("Index", new { productPage = i });
+                    tag.InnerHtml.AppendHtml(i.ToString());
+                    result.InnerHtml.AppendHtml(tag);
+                }
+                output.Content.AppendHtml(result.InnerHtml);
+            }
+        }
+    }
+}
