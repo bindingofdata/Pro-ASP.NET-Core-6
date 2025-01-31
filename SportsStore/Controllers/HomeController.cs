50b18b3bba98e45baea06cfec062903651e4ea71
Hunter Curren <hcurren82@gmail.com>
2025-01-31 10:58:55 -0800

    Completed navigation support: Selected category is highlighted and pagination shows appropriate number of pages per category.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -28,7 +28,9 @@ namespace SportsStore.Controllers
                 {
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
-                    TotalItems = _repository.Products.Count()
+                    TotalItems = string.IsNullOrWhiteSpace(category) ?
+                        _repository.Products.Count() :
+                        _repository.Products.Where(product => product.Category == category).Count()
                 },
                 CurrentCategory = category
             });
