8de00961df351b6ed0df678ee96e1e4472dca84c
Hunter Curren <hcurren82@gmail.com>
2025-01-31 12:05:21 -0800

    Continue shopping cart support: Added support for sessions, and initial cart implementation and unit testing.

---
+++ SportsStore/Models/Cart.cs
@@ -0,0 +1,42 @@
+﻿namespace SportsStore.Models
+{
+    public sealed class Cart
+    {
+        public List<CartLine> Lines { get; set; } = [];
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine? line = Lines
+                .Where(cartLine => cartLine.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product) =>
+            Lines.RemoveAll(cartLine => cartLine.Product.ProductID == product.ProductID);
+
+        public decimal ComputeTotalValue() =>
+            Lines.Sum(cartLine => cartLine.Product.Price * cartLine.Quantity);
+
+        public void Clear() => Lines.Clear();
+    }
+
+    public sealed class CartLine
+    {
+        public int CartLineId { get; set; }
+        public Product Product { get; set; } = new();
+        public int Quantity { get; set; }
+    }
+}
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -92,11 +92,13 @@ namespace SportsStore
             builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
             builder.Services.AddRazorPages();
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession();
 
             var app = builder.Build();
 
-            //app.MapGet("/", () => "Hello World!");
             app.UseStaticFiles();
+            app.UseSession();
 
             app.MapControllerRoute("catpage",
                 "{category}/Page{productPage:int}",
