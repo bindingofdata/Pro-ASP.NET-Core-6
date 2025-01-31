255fe66514e845b381ac899895f20aa2b4708a9a
Hunter Curren <hcurren82@gmail.com>
2025-01-31 09:41:57 -0800

    Continue navigation support: Added category navigation menu.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public sealed class NavigationMenuViewComponent : ViewComponent
+    {
+        private readonly IStoreRepository _repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IViewComponentResult Invoke() => View(_repository.Products
+            .Select(product => product.Category)
+            .Distinct()
+            .OrderBy(category => category));
+    }
+}
