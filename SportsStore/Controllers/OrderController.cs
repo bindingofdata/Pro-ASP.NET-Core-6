f55d0a0f697cbe59e23604fad37bd94436fcadbf
Hunter Curren <hcurren82@gmail.com>
2025-02-04 11:06:10 -0800

    Continue shopping cart support: Added checkout page and backing classes.

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public sealed class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
