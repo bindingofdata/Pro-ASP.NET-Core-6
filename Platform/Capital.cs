30ceff7190cc05674054f472d40e2182ee8847f6
Hunter Curren <hcurren82@gmail.com>
2025-03-04 15:52:04 -0800

    Updated redirect in Capital to use generated route.

--- Platform/Capital.cs
+++ Platform/Capital.cs
@@ -15,7 +15,12 @@
                     capital = "Paris";
                     break;
                 case "monoco":
-                    context.Response.Redirect($"/population/{country}");
+                    LinkGenerator? generator = context.RequestServices.GetService<LinkGenerator>();
+                    string? url = generator?.GetPathByRouteValues(context, "population", new { city = country });
+                    if (url != null)
+                    {
+                        context.Response.Redirect(url);
+                    }
                     return;
             }
             if (capital != null)
