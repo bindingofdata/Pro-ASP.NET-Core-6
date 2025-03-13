b50737576ee081b6f63c69d8afee707b23e2b534
Hunter Curren <hcurren82@gmail.com>
2025-03-13 15:16:25 -0700

    Using a service in a middleware class example

--- Platform/WeatherMiddleware.cs
+++ Platform/WeatherMiddleware.cs
@@ -1,18 +1,22 @@
-﻿namespace Platform
+﻿using Platform.Services;
+
+namespace Platform
 {
     public sealed class WeatherMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly IResponseFormatter _formatter;
 
-        public WeatherMiddleware(RequestDelegate next)
+        public WeatherMiddleware(RequestDelegate next, IResponseFormatter formatter)
         {
             _next = next;
+            _formatter = formatter;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             if (string.Equals(context.Request.Path, "/middleware/class"))
-                await context.Response.WriteAsync("Middleware Class: It is raining in London");
+                await _formatter.Format(context, "Middleware Class: It is raining in London");
             else
                 await _next(context);
         }
