34d651d6b206ec81269bba76732a951f32766d3d
Hunter Curren <hcurren82@gmail.com>
2025-03-24 14:20:10 -0700

    Reset for next section.

--- Platform/Program.cs
+++ Platform/Program.cs
@@ -12,24 +12,24 @@ namespace Platform
             var builder = WebApplication.CreateBuilder(args);
 
             #region Configuring the Session Service and Middleware
-            builder.Services.AddDistributedMemoryCache();
-            builder.Services.AddHttpsRedirection(opts =>
-            {
-                opts.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
-                opts.HttpsPort = 5500;
-            });
-            builder.Services.AddSession(opts =>
-            {
-                opts.IdleTimeout = TimeSpan.FromMinutes(30);
-                opts.Cookie.IsEssential = true;
-            });
+            //builder.Services.AddDistributedMemoryCache();
+            //builder.Services.AddHttpsRedirection(opts =>
+            //{
+            //    opts.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+            //    opts.HttpsPort = 5500;
+            //});
+            //builder.Services.AddSession(opts =>
+            //{
+            //    opts.IdleTimeout = TimeSpan.FromMinutes(30);
+            //    opts.Cookie.IsEssential = true;
+            //});
             #endregion
             #region Enabling HTTP Strict Transport Security
-            builder.Services.AddHsts(opts =>
-            {
-                opts.MaxAge = TimeSpan.FromDays(1);
-                opts.IncludeSubDomains = true;
-            });
+            //builder.Services.AddHsts(opts =>
+            //{
+            //    opts.MaxAge = TimeSpan.FromDays(1);
+            //    opts.IncludeSubDomains = true;
+            //});
             #endregion
             #region Enabling Cookie Consent Checking
             //builder.Services.Configure<CookiePolicyOptions>(opts =>
@@ -93,26 +93,32 @@ namespace Platform
             var app = builder.Build();
 
             #region enabling HTTP Strict Transport Security
-            if (app.Environment.IsProduction())
-            {
-                app.UseHsts();
-            }
+            //if (app.Environment.IsProduction())
+            //{
+            //    app.UseHsts();
+            //}
+
+            //app.MapFallback(async context =>
+            //{
+            //    await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}\n");
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
             #endregion
             #region configuring the Session Service and Middleware
-            app.UseHttpsRedirection();
-            app.UseSession();
-            app.UseMiddleware<ConsentMiddleware>();
+            //app.UseHttpsRedirection();
+            //app.UseSession();
+            //app.UseMiddleware<ConsentMiddleware>();
             #endregion
             #region using Session data example
-            app.MapGet("/session", async context =>
-            {
-                int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
-                int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
-                context.Session.SetInt32("counter1", counter1);
-                context.Session.SetInt32("counter2", counter2);
-                await context.Session.CommitAsync();
-                await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
-            });
+            //app.MapGet("/session", async context =>
+            //{
+            //    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+            //    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+            //    context.Session.SetInt32("counter1", counter1);
+            //    context.Session.SetInt32("counter2", counter2);
+            //    await context.Session.CommitAsync();
+            //    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+            //});
             #endregion
             #region enabling cookie consent checking
             //app.UseCookiePolicy();
@@ -341,12 +347,6 @@ namespace Platform
             //// custom class-based middleware
             //app.UseMiddleware<QueryStringMiddleWare>();
             #endregion
-
-            app.MapFallback(async context =>
-            {
-                await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}\n");
-                await context.Response.WriteAsync("Hello World!");
-            });
 
             app.Run();
         }
