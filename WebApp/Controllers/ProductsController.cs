1d69c337cc5989029af8002c3b464a60c2fd5c3a
Hunter Curren <hcurren82@gmail.com>
2025-03-28 13:04:23 -0700

    Basic controllers example.

---
+++ WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    public sealed class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return [
+                new Product { ProductId = 1, Name = "Product 1" },
+                new Product { ProductId = 2, Name = "Product 2" }];
+        }
+
+        [HttpGet("{id}")]
+        public Product GetProduct()
+        {
+            return new Product() { ProductId = 1, Name = "Product 1" };
+        }
+    }
+}
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -13,34 +13,46 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.EnableSensitiveDataLogging();
 });
 
+#region Controller examples
+builder.Services.AddControllers();
+#endregion
+
 var app = builder.Build();
 
-app.MapGet($"{BASE_URL}/{{id}}", async (HttpContext context, DataContext data) =>
-{
-    string? id = context.Request.RouteValues["id"] as string;
-    if (!string.IsNullOrWhiteSpace(id))
-    {
-        Product? product = await data.Products.FindAsync(long.Parse(id));
-        if (product == null)
-            context.Response.StatusCode = StatusCodes.Status404NotFound;
-        else
-            await context.Response.WriteAsJsonAsync(product);
-    }
-});
+#region Controller examples
+app.MapControllers();
+#endregion
+#region Minimal API examples
+//// get a specific product
+//app.MapGet($"{BASE_URL}/{{id}}", async (HttpContext context, DataContext data) =>
+//{
+//    string? id = context.Request.RouteValues["id"] as string;
+//    if (!string.IsNullOrWhiteSpace(id))
+//    {
+//        Product? product = await data.Products.FindAsync(long.Parse(id));
+//        if (product == null)
+//            context.Response.StatusCode = StatusCodes.Status404NotFound;
+//        else
+//            await context.Response.WriteAsJsonAsync(product);
+//    }
+//});
 
-app.MapGet(BASE_URL, async (HttpContext context, DataContext data) =>
-    await context.Response.WriteAsJsonAsync(data.Products));
+//// get all products
+//app.MapGet(BASE_URL, async (HttpContext context, DataContext data) =>
+//    await context.Response.WriteAsJsonAsync(data.Products));
 
-app.MapPost(BASE_URL, async (HttpContext context, DataContext data) =>
-{
-    Product? product = await context.Request.ReadFromJsonAsync<Product>();
-    if (product != null)
-    {
-        await data.AddAsync(product);
-        await data.SaveChangesAsync();
-        context.Response.StatusCode = StatusCodes.Status201Created;
-    }
-});
+//// add a new product
+//app.MapPost(BASE_URL, async (HttpContext context, DataContext data) =>
+//{
+//    Product? product = await context.Request.ReadFromJsonAsync<Product>();
+//    if (product != null)
+//    {
+//        await data.AddAsync(product);
+//        await data.SaveChangesAsync();
+//        context.Response.StatusCode = StatusCodes.Status201Created;
+//    }
+//});
+#endregion
 
 app.MapGet("/", () => "Hello World!");
 
