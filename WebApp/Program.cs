07352e3826e21dced703cec00b39025d8e2f6c48
Hunter Curren <hcurren82@gmail.com>
2025-03-28 12:09:28 -0700

    Basic web service with minimal API example.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using WebApp;
 using WebApp.Models;
 
+const string BASE_URL = "api/products";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(opts =>
@@ -13,7 +15,32 @@ builder.Services.AddDbContext<DataContext>(opts =>
 
 var app = builder.Build();
 
-app.UseMiddleware<TestMiddleware>();
+app.MapGet($"{BASE_URL}/{{id}}", async (HttpContext context, DataContext data) =>
+{
+    string? id = context.Request.RouteValues["id"] as string;
+    if (!string.IsNullOrWhiteSpace(id))
+    {
+        Product? product = await data.Products.FindAsync(long.Parse(id));
+        if (product == null)
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+        else
+            await context.Response.WriteAsJsonAsync(product);
+    }
+});
+
+app.MapGet(BASE_URL, async (HttpContext context, DataContext data) =>
+    await context.Response.WriteAsJsonAsync(data.Products));
+
+app.MapPost(BASE_URL, async (HttpContext context, DataContext data) =>
+{
+    Product? product = await context.Request.ReadFromJsonAsync<Product>();
+    if (product != null)
+    {
+        await data.AddAsync(product);
+        await data.SaveChangesAsync();
+        context.Response.StatusCode = StatusCodes.Status201Created;
+    }
+});
 
 app.MapGet("/", () => "Hello World!");
 
