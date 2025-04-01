e191a3235ea8ff282943c1a3736bf6573e157f35
Hunter Curren <hcurren82@gmail.com>
2025-04-01 11:44:08 -0700

    Basic content formatting - Default content policy example.

---
+++ WebApp/Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ContentController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ContentController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public async Task<Product> GetObject() =>
+            await _context.Products.FirstAsync();
+    }
+}
