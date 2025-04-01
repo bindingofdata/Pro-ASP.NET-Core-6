4fa7d71e381a1edcd34dcc6b4eeed25120598a0e
Hunter Curren <hcurren82@gmail.com>
2025-04-01 10:47:25 -0700

    Setup project for Chapter 20.

---
+++ WebApp/Controllers/SuppliersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class SuppliersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuppliersController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Supplier?> GetSupplier(long id)
+        {
+            return await _context.Suppliers.FindAsync(id);
+        }
+    }
+}
