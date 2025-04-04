7fea979742183270968fddfad3950f485838007c
Hunter Curren <hcurren82@gmail.com>
2025-04-04 12:59:58 -0700

    Example use of _ViewImports for pages.

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using WebApp.Models;
+
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly DataContext _context;
+
+        public Product? Product { get; set; }
+
+        public IndexModel(DataContext dataContext)
         {
+            _context = dataContext;
+        }
+
+        public async Task OnGetAsync(long id = 1)
+        {
+            Product = await _context.Products.FindAsync(id);
         }
     }
 }
