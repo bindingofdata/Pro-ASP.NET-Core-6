c6c1a769eb00726a7e6105399902ffcc0c2ed0b5
Hunter Curren <hcurren82@gmail.com>
2025-04-07 16:52:59 -0700

    Asynchronous view component example.

---
+++ WebApp/Components/PageSize.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Components
+{
+    public sealed class PageSize : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("http://apress.com");
+            return View(response.Content.Headers.ContentLength);
+        }
+    }
+}
