428af01d587bf4aa6c66ad90d2dabffe5e3f388a
Hunter Curren <hcurren82@gmail.com>
2025-04-07 14:45:46 -0700

    Creating and using view component example.

---
+++ WebApp/Components/CitySummary.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public sealed class CitySummary : ViewComponent
+    {
+        private readonly CitiesData _data;
+
+        public CitySummary(CitiesData data)
+        {
+            _data = data;
+        }
+
+        public string Invoke()
+        {
+            return $"{_data.Cities.Count} cities, "
+                + $"{_data.Cities.Sum(city => city.Population)}";
+        }
+    }
+}
