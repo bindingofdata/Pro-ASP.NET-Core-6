3d0538872da937ce19edcec7e5e3e58035386bc2
Hunter Curren <hcurren82@gmail.com>
2025-04-09 16:15:44 -0700

    Coordinating between tag helpers example.

---
+++ WebApp/TagHelpers/CoordinatingTagHelpers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("tr", Attributes = "theme")]
+    public sealed class RowTagHelper : TagHelper
+    {
+        public string Theme { get; set; } = string.Empty;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            context.Items["theme"] = Theme;
+        }
+    }
+
+    [HtmlTargetElement("th")]
+    [HtmlTargetElement("td")]
+    public sealed class CellTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context.Items.ContainsKey("theme"))
+            {
+                output.Attributes.SetAttribute("class", $"bg-{context.Items["theme"]} text-white");
+            }
+        }
+    }
+}
