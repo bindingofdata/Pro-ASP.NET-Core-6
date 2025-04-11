5a7c1d8ccc40544cb34063f366ed3c17573a6873
Hunter Curren <hcurren82@gmail.com>
2025-04-11 12:04:56 -0700

    Creating a controller to handle forms example.

---
+++ WebApp/Controllers/FormController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public sealed class FormController : Controller
+    {
+        private readonly DataContext _context;
+
+        public FormController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(long id = 1)
+        {
+            return View("Form", await _context.Products.FindAsync(id));
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm()
+        {
+            foreach (string key in Request.Form.Keys
+                .Where(key => !key.StartsWith("_")))
+            {
+                TempData[key] = string.Join(", ", Request.Form[key].ToArray());
+            }
+            return RedirectToAction(nameof(Results));
+        }
+
+        public IActionResult Results()
+        {
+            return View();
+        }
+    }
+}
