be6e2d972a5131c32621215177958724980f3903
Hunter Curren <hcurren82@gmail.com>
2025-04-11 11:23:14 -0700

    Reset project for Chapter 27 - Using the Forms Tag Helpers.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -65,8 +65,9 @@ builder.Services.AddSingleton<CitiesData>();
 var app = builder.Build();
 
 app.UseStaticFiles();
-app.MapControllers();
-app.MapDefaultControllerRoute();
+//app.MapControllers();
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute("forms", "controllers/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 #region Razor Pages examples
