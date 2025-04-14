3c631482fce7b21ec1e6af9fa6cd8f36ad2089eb
Hunter Curren <hcurren82@gmail.com>
2025-04-14 16:08:34 -0700

    Displaying data from related values using EF Core Include statements.

--- WebApp/Controllers/FormController.cs
+++ WebApp/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using WebApp.Models;
 
@@ -15,7 +16,10 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
-            return View("Form", await _context.Products.FindAsync(id));
+            return View("Form", await _context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstAsync(p => p.ProductId == id));
         }
 
         [HttpPost]
