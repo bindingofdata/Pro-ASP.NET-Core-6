2cf4f987e3a8d9102890ef09c7145227fea706ef
Hunter Curren <hcurren82@gmail.com>
2025-04-15 12:54:18 -0700

    Selectively binding properties in the model class example.

--- WebApp/Models/Product.cs
+++ WebApp/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
@@ -12,6 +14,7 @@ namespace WebApp.Models
         public string Name { get; set; } = string.Empty;
 
         [Column(TypeName = "decimal(8, 2)")]
+        [BindNever]
         public decimal Price { get; set; }
 
         public long CategoryId { get; set; }
