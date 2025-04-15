2ad4707518b3836137ddb55dfb39d01475ef42aa
Hunter Curren <hcurren82@gmail.com>
2025-04-15 12:52:46 -0700

    Selectively binding properties example.

--- WebApp/Controllers/FormController.cs
+++ WebApp/Controllers/FormController.cs
@@ -28,9 +28,11 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult SubmitForm(Product product)
+        public IActionResult SubmitForm([Bind("Name", "Category")]Product product)
         {
-            TempData["product"] = JsonSerializer.Serialize(product);
+            TempData["name"] = product.Name;
+            TempData["price"] = product.Price.ToString();
+            TempData["category name"] = product.Category?.Name;
             return RedirectToAction(nameof(Results));
         }
 
