5351422695399020387e51fb45b11eb66648ee0f
Hunter Curren <hcurren82@gmail.com>
2025-04-16 17:45:18 -0700

    Remote validation in Razor pages example.

--- WebApp/Controllers/ValidationController.cs
+++ WebApp/Controllers/ValidationController.cs
@@ -16,19 +16,26 @@ namespace WebApp.Controllers
         }
 
         [HttpGet("categorykey")]
-        public bool CategoryKey(string categoryId)
+        public bool CategoryKey(string? categoryId, [FromQuery] KeyTarget target)
         {
             long keyVal;
-            return long.TryParse(categoryId, out keyVal)
+            return long.TryParse(categoryId ?? target.CategoryId, out keyVal)
                 && _context.Categories.Any(c => c.CategoryId == keyVal);
         }
 
         [HttpGet("supplierkey")]
-        public bool SupplierKey(string supplierId)
+        public bool SupplierKey(string? supplierId, [FromQuery] KeyTarget target)
         {
             long keyVal;
-            return long.TryParse(supplierId, out keyVal)
+            return long.TryParse(supplierId ?? target.SupplierId, out keyVal)
                 && _context.Suppliers.Any(s => s.SupplierId == keyVal);
         }
+    }
+
+    [Bind(Prefix = "Product")]
+    public sealed class KeyTarget
+    {
+        public string? CategoryId { get; set; }
+        public string? SupplierId { get; set; }
     }
 }
