39a41f92414857f0e919e087f66ca18589a7a2ea
Hunter Curren <hcurren82@gmail.com>
2025-04-16 17:01:20 -0700

    Creating a custom property validation attribute example.

---
+++ WebApp/Validation/PrimaryKeyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Validation
+{
+    public sealed class PrimaryKeyAttribute : ValidationAttribute
+    {
+        public Type? ContextType { get; set; }
+        public Type? DataType { get; set; }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (ContextType != null
+                && DataType != null)
+            {
+                DbContext? context = validationContext.GetService(ContextType) as DbContext;
+                if (context != null
+                    && context.Find(DataType, value) == null)
+                {
+                    return new ValidationResult(ErrorMessage ??
+                        "Enter an existing key value");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
