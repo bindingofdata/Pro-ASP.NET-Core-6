712b3b940f1210590b23319092068b849ec6451b
Hunter Curren <hcurren82@gmail.com>
2025-04-17 12:36:49 -0700

    Basic using filters example.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -2,11 +2,19 @@
 
 namespace WebApp.Controllers
 {
+    [RequireHttps]
     public sealed class HomeController : Controller
     {
+        //[RequireHttps]
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller.");
+        }
+
+        //[RequireHttps]
+        public IActionResult Secure()
+        {
+            return View("Message", "This is the Secure action on the Home controller");
         }
     }
 }
