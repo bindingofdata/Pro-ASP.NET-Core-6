7fbbe5168ee5f8d9c131d2f79f6186773f013310
Hunter Curren <hcurren82@gmail.com>
2025-04-17 12:21:05 -0700

    Reset project for chapter 30 Using Filters: Added new base files and enabled HTTPS.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Message", "This is the Index action on the Home controller.");
+        }
+    }
+}
