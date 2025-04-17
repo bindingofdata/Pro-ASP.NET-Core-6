7e0fd5e72e846ebb7469fced6a825e8ab2bbc237
Hunter Curren <hcurren82@gmail.com>
2025-04-16 17:19:44 -0700

    Using custom model validation in a Razor page.

---
+++ WebApp/Validation/ModelStateExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace WebApp.Validation
+{
+    public static class ModelStateExtensions
+    {
+        public static void PromotePropertyErrors(this ModelStateDictionary modelState, string propertyName)
+        {
+            foreach (KeyValuePair<string, ModelStateEntry> errors in modelState)
+            {
+                if (errors.Key == propertyName
+                    && errors.Value.ValidationState == ModelValidationState.Invalid)
+                {
+                    foreach (ModelError error in errors.Value.Errors)
+                    {
+                        modelState.AddModelError(string.Empty, error.ErrorMessage);
+                    }
+                }
+            }
+        }
+    }
+}
