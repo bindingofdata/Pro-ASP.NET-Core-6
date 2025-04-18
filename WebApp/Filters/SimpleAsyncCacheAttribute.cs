3a6513643317d1c46d7c03f295dd6d8b7de54f04
Hunter Curren <hcurren82@gmail.com>
2025-04-18 12:56:53 -0700

    Creating an Async Resource filter example.

---
+++ WebApp/Filters/SimpleAsyncCacheAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+    public sealed class SimpleAsyncCacheAttribute : Attribute, IAsyncResourceFilter
+    {
+        private readonly Dictionary<PathString, IActionResult> _cachedResponses = new();
+
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            PathString path = context.HttpContext.Request.Path;
+            if (_cachedResponses.TryGetValue(path, out IActionResult? value))
+            {
+                context.Result = value;
+                _cachedResponses.Remove(path);
+            }
+            else
+            {
+                ResourceExecutedContext execContext = await next();
+                if (execContext.Result != null)
+                {
+                    _cachedResponses.Add(path, execContext.Result);
+                }
+            }
+        }
+    }
+}
