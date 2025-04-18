0b669caa304234f0b396b95735a36edaac1636b3
Hunter Curren <hcurren82@gmail.com>
2025-04-18 12:49:36 -0700

    Creating a basic Resource filter example.

---
+++ WebApp/Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+    public sealed class SimpleCacheAttribute : Attribute, IResourceFilter
+    {
+        private readonly Dictionary<PathString, IActionResult> _cachedResponses = new();
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            PathString path = context.HttpContext.Request.Path;
+            if (_cachedResponses.TryGetValue(path, out IActionResult? value))
+            {
+                context.Result = value;
+                _cachedResponses.Remove(path);
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            if (context.Result != null)
+            {
+                _cachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+            }
+        }
+    }
+}
