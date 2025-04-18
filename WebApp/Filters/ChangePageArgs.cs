a912503fbb1f0aad33d163c22e4b3a0b4af9bfd0
Hunter Curren <hcurren82@gmail.com>
2025-04-18 13:29:44 -0700

    Creating a basic Page filter example.

---
+++ WebApp/Filters/ChangePageArgs.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApp.Filters
+{
+    public sealed class ChangePageArgs : Attribute, IPageFilter
+    {
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+            // do nothing
+        }
+
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            if (context.HandlerArguments.ContainsKey("message1"))
+            {
+                context.HandlerArguments["message1"] = "New message";
+            }
+        }
+
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+            // do nothing
+        }
+    }
+}
