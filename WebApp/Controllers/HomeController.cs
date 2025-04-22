4af103a8ad63dde8b7f6f36fc2cc326c8dfdc30a
Hunter Curren <hcurren82@gmail.com>
2025-04-22 16:41:09 -0700

    Basic Default Filter Order example.

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,52 +5,14 @@ using WebApp.Filters;
 
 namespace WebApp.Controllers
 {
-    [ResultDiagnostics]
-    //[GuidResponse]
-    //[GuidResponse]
+    [Message("This is the controller-scoped filter")]
     public sealed class HomeController : Controller
     {
-        //[RequireHttps]
+        [Message("This is the first action-scoped filter")]
+        [Message("This is the second action-scoped filter")]
         public IActionResult Index()
         {
             return View("Message", "This is the Index action on the Home controller.");
-        }
-
-        //[RequireHttps]
-        public IActionResult Secure()
-        {
-            return View("Message", "This is the Secure action on the Home controller");
-        }
-
-        //[ChangeArg]
-        public IActionResult Messages(string message1, string message2 = "None")
-        {
-            return View("Message", $"{message1}, {message2}");
-        }
-
-        public override void OnActionExecuting(ActionExecutingContext context)
-        {
-            if (context.ActionArguments.ContainsKey("message1"))
-            {
-                context.ActionArguments["message1"] = "New message";
-            }
-        }
-
-        [RangeException]
-        public ViewResult GenerateException(int? id)
-        {
-            if (!id.HasValue)
-            {
-                throw new ArgumentNullException(nameof(id));
-            }
-            else if (id.Value > 10)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-            else
-            {
-                return View("Message", $"The value is {id}");
-            }
         }
     }
 }
---
+++ WebApp/Filters/MessageAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.VisualBasic;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace WebApp.Filters
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]
+    public sealed class MessageAttribute : Attribute, IAsyncAlwaysRunResultFilter
+    {
+        private int _counter = 0;
+        private string _message;
+
+        public MessageAttribute(string message) => _message = message;
+
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            Dictionary<string, string> resultData;
+            if (context.Result is ViewResult viewResult
+                && viewResult.ViewData.Model is Dictionary<string, string> data)
+            {
+                resultData = data;
+            }
+            else
+            {
+                resultData = new Dictionary<string, string>();
+                context.Result = new ViewResult()
+                {
+                    ViewName = "/Views/Shared/Message.cshtml",
+                    ViewData = new ViewDataDictionary(
+                        new EmptyModelMetadataProvider(),
+                        new ModelStateDictionary())
+                    {
+                        Model = resultData
+                    }
+                };
+            }
+
+            while (resultData.ContainsKey($"Message_{_counter}"))
+            {
+                _counter++;
+            }
+            resultData[$"Message_{_counter}"] = _message;
+            await next();
+        }
+    }
+}
