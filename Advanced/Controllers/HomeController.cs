1e7e12ec7cc402769c4b06e0b7e8092e2c28f7cc
Hunter Curren <hcurren82@gmail.com>
2025-04-25 14:19:31 -0700

    Creating a controller and view.

---
+++ Advanced/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Advanced.Models;
+using Advanced.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+
+using System.Data.Entity;
+
+namespace Advanced.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        private readonly DataContext _context;
+
+        public HomeController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index([FromQuery] string selectedCity)
+        {
+            return View(new PeopleListViewModel
+            {
+                People = _context.People
+                    .Include(p => p.Department)
+                    .Include(p => p.Location),
+                Cities = _context.Locations.Select(l => l.City).Distinct(),
+                SelectedCity = selectedCity
+            });
+        }
+    }
+}
---
+++ Advanced/ViewModels/PeopleListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Advanced.Models;
+
+namespace Advanced.ViewModels
+{
+    public sealed class PeopleListViewModel
+    {
+        public IEnumerable<Person> People { get; set; } = Enumerable.Empty<Person>();
+        public IEnumerable<string> Cities { get; set; } = Enumerable.Empty<string>();
+        public string SelectedCity { get; set; } = string.Empty;
+
+        public string GetClass(string? city) =>
+            SelectedCity == city ? "bg-info text-white" : string.Empty;
+    }
+}
