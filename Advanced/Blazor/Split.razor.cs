8dd6eab4d4c73085ef3340a5a6ae123a581ce03f
Hunter Curren <hcurren82@gmail.com>
2025-05-12 12:02:21 -0700

    Using Class Files to Define Components: Using code behind example.

---
+++ Advanced/Blazor/Split.razor.cs
@@ -0,0 +1,16 @@
+﻿using Advanced.Models;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Advanced.Blazor
+{
+    public partial class Split
+    {
+        [Inject]
+        public DataContext? Context { get; set; }
+
+        public IEnumerable<string> Names =>
+            Context?.People.Select(person => person.FirstName)
+            ?? Enumerable.Empty<string>();
+    }
+}
